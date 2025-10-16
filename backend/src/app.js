53562c479eda213b1bc2c2475e8301cae1d54064
ujwal-devpX <sujwals2005@gmail.com>
2025-10-16 21:34:07 +0530

    router updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 import authRouth from "./routers/auth.router.js";
 
