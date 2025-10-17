2cef00f4ee960b96b92b9f171ea3cc38d385b360
ujwal-devpX <sujwals2005@gmail.com>
2025-10-17 22:59:15 +0530

    updated auth controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,7 +2,8 @@ import { APIerror } from "../utils/apierror.js";
 import { APIresponse } from "../utils/apiresponse.js";
 import { User } from "../models/user.model.js";
 import cloudUpload from "../utils/cloudinary.js";
-import { v2 as cloudinary } from "cloudinary";
+import profileFunction from "../utils/profile.js";
+import jwt from "jsonwebtoken";
 
 const generateTokens = async (userID) => {
   const user = await User.findById(userID);
@@ -28,13 +29,13 @@ export const register = async (req, res) => {
     if (userExist) {
       throw new APIerror(400, "User already exist");
     }
-
+    const photo = await profileFunction();
     const user = await User.create({
       username: username.toLowerCase(),
       fullname,
       email,
       password,
-      profilePic: profileDefault.url,
+      profilePic: photo.url,
     });
 
     const userInfo = await User.findById(user._id).select("-password");
@@ -94,18 +95,18 @@ export const login = async (req, res) => {
       .cookie("refreshToken", refreshToken, options)
       .json(new APIresponse(200, userLogin, "Login successfully"));
   } catch (error) {
-    // if (error instanceof APIerror) {
-    //   return res.status(error.statusCode).json({
-    //     success: false,
-    //     message: error.message,
-    //   });
-    // }
+    if (error instanceof APIerror) {
+      return res.status(error.statusCode).json({
+        success: false,
+        message: error.message,
+      });
+    }
 
-    // res.status(500).json({
-    //   success: false,
-    //   message: "INTERNAL SERVER ERROR",
-    // });
-    throw new APIerror(401, error?.message || "Error while login");
+    res.status(500).json({
+      success: false,
+      message: "INTERNAL SERVER ERROR",
+    });
+    // throw new APIerror(401, error?.message || "Error while login");
   }
 };
 
@@ -135,6 +136,145 @@ export const logout = async (req, res) => {
       .json(new APIresponse(200, {}, "User logged out successfully"));
   } catch (error) {
     console.log("Error while logout", error);
-    throw new APIerror(404, "Invlaid creditials");
+    // throw new APIerror(404, "Invlaid creditials");
+    if (error instanceof APIerror) {
+      return res.status(error.statusCode).json({
+        success: false,
+        message: error.message,
+      });
+    }
+
+    res.status(500).json({
+      success: false,
+      message: "Erro while logout",
+    });
+  }
+};
+
+export const refreshAccessToken = async (req, res) => {
+  try {
+    const incomingToken = req.cookies.refreshToken || req.body.refreshToken;
+    if (!incomingToken) {
+      throw new APIerror(400, "Unauthorized request");
+    }
+
+    const decodeToken = jwt.verify(
+      incomingToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    const user = await User.findById(decodeToken?._id);
+    if (!user) {
+      throw new APIerror(400, "Invalid refresh token");
+    }
+    if (incomingToken != user.refreshToken) {
+      throw new APIerror(400, "Refresh token didnt match");
+    }
+
+    const options = {
+      httpOnly: true,
+      secured: true,
+    };
+
+    const { accessToken, refreshToken } = generateTokens(user._id);
+    const userInfo = await User.findById(user._id).select(
+      "-password -refreshToken"
+    );
+
+    res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", refreshToken, options)
+      .json(new APIresponse(200, userInfo, "Token refreshed successfully"));
+  } catch (error) {
+    // console.log("Error while refreshing access token :", error);
+    if (error instanceof APIerror) {
+      return res.status(error.statusCode).json({
+        success: false,
+        message: error.message,
+      });
+    }
+
+    res.status(500).json({
+      success: false,
+      message: "Error while refreshing token",
+    });
+  }
+};
+
+export const updateProfilePic = async (req, res) => {
+  try {
+    const profilePath = req.file?.path;
+    if (!profilePath) {
+      throw new APIerror(400, "No path exist");
+    }
+
+    const uploading = await cloudUpload(profilePath);
+
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $set: {
+          profilePic: uploading.url,
+        },
+      },
+      {
+        new: true,
+      }
+    ).select("-password -refreshToken");
+
+    return res
+      .status(200)
+      .json(new APIresponse(200, user, "Profile pic updated "));
+  } catch (error) {
+    console.log("Error while updating profile", error);
+    // throw new APIerror(400, error?.message || "Error while updating profile");
+    if (error instanceof APIerror) {
+      return res.status(error.statusCode).json({
+        success: false,
+        message: error.message,
+      });
+    }
+
+    res.status(500).json({
+      success: false,
+      message: "Error while updating profile pic",
+    });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { fullname, email } = req.body;
+    if (!fullname || !email) {
+      throw new APIerror(400, "ALl the field are required");
+    }
+
+    const user = await User.findByIdAndUpdate(
+      req.user?._id,
+      {
+        $set: {
+          fullname: fullname,
+          email: email,
+        },
+      },
+      { new: true }
+    ).select("-password");
+    return res
+      .status(200)
+      .json(new APIresponse(200, user, "User updated successfully"));
+  } catch (error) {
+    console.log("Error while updating profile", error);
+    if (error instanceof APIerror) {
+      return res.status(error.statusCode).json({
+        success: false,
+        message: error.message,
+      });
+    }
+
+    res.status(500).json({
+      success: false,
+      message: "Error while updating profile",
+    });
   }
 };
