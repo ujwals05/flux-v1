5b1e2e7c60ad8b8dc61f96123241c35d3fa7f72b
ujwal-devpX <sujwals2005@gmail.com>
2025-10-23 01:26:38 +0530

    completed auth related state management

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,11 +1,14 @@
 import { create } from "zustand";
 import { axiosInstance } from "../utils/axios.js";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 export const useAuthStore = create((set) => ({
   authUser: null,
   isSigningUp: false,
   isLoggingIn: false,
   isUpdatingProfile: false,
+  // isUpdatingProfilePic: false,
   isCheckingAuth: true,
 
   checkAuth: async () => {
@@ -20,5 +23,64 @@ export const useAuthStore = create((set) => ({
     }
   },
 
-  signup: async (data) => {},
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/users/signup", data);
+      // set({ authUser: res.data });
+      toast.success("Account created successfully! Please log in.");
+    } catch (error) {
+      console.log("Error occured while signup", error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/users/login", data);
+      set({ authUser: res.data });
+      toast.success("Successfully logged in");
+    } catch (error) {
+      console.log("Error while login", error);
+      toast.error(error.response.data.message)
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/users/logout");
+      set({ authUser: null });
+      toast.success("Successfully Logged out");
+    } catch (error) {
+      console.log("Error while logout", error);
+      toast.error(error.response.message.data);
+    }
+  },
+
+  updateProfilePic: async (formData) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.post(
+        "/users/updateProfilePic",
+        formData,
+        {
+          headers: { "Content-Type": "multipart/form-data" },
+        }
+      );
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("Error while updating profile", error);
+      toast.error(error.response?.data?.message || "Error updating profile");
+    } finally {
+      set({ isUpdatingProfile: false });
+    }
+  },
+
+  updateProfile: async () => {},
 }));
