3ce83901a0d53ffde0b1a863cf76dbb322098bf7
ujwal-devpX <sujwals2005@gmail.com>
2025-10-24 12:57:46 +0530

    Componets for chat container and home page

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore.js";
+import ChatHeader from "./ChatHeader.jsx";
+import MessageInput from "./MessageInput.jsx";
+import MessageSkeleton from "./MessageSkeleton.jsx";
 
 const ChatContainer = () => {
-  return (
-    <div>ChatContainer</div>
-  )
-}
+  const { getMessages, isMessageLoading, messages, selectedUser } =
+    useChatStore();
 
-export default ChatContainer
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
+
+  if(isMessageLoading) return <div className="flex-1 flex flex-col overflow-auto">
+    <ChatHeader/>
+    <MessageSkeleton/>
+    <MessageInput/>
+  </div>;
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+
+      <p>Messages</p>
+
+      <MessageInput />
+    </div>
+  );
+};
+
+export default ChatContainer;
