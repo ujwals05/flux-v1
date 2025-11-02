77fb2ea42891636afab2493ed49fd44ba7f79ea3
ujwal-devpX <sujwals2005@gmail.com>
2025-11-02 20:46:03 +0530

    updated file for deploy

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,9 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:8001" : "/";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:8001"
+    : "https://flux-backend.vercel.app";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:8001/api" // when local
-      : "/api", 
+      : "https://flux-backend.vercel.app/api",
   withCredentials: true,
 });
